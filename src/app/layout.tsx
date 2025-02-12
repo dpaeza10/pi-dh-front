2b2b2cde694f0d2a577985a329825a1cf7b915dd
dpaeza10 <daniela.paez@inchcape.com>
2025-02-12 17:09:16 -0500

    fix bugs...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,15 @@ import { Geist, Geist_Mono } from "next/font/google";
 import  ReactQueryProvider from "@/lib/react-query";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
