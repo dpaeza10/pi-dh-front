aa51b36353aada31ab42f8028b7cdb9247f05f31
drbh2020 <berastaindiego@gmail.com>
2025-02-13 18:58:39 -0500

    refactor: Footer and Navbar based on prettier formatting

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,21 @@
-"use client";
+'use client'
 
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Image from 'next/image';
-import Box from '@mui/material/Box';
+import { AppBar, Box, Toolbar, Typography } from '@mui/material'
+import Image from 'next/image'
 
 const Footer = () => {
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <AppBar position="static" sx={{ backgroundColor: 'var(--darkBlue)' }}>
-                <Toolbar>
-                    <Image src="/Logo.svg" alt="Logo" width={70} height={60} />
-                    <Typography variant="body1" color="inherit" sx={{ fontSize: "12px" }}>
-                        © 2025 Carato
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
-};
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="static" sx={{ backgroundColor: 'var(--darkBlue)' }}>
+        <Toolbar>
+          <Image src="/Logo.svg" alt="Logo" width={70} height={60} />
+          <Typography variant="body1" color="inherit" sx={{ fontSize: '12px' }}>
+            © 2025 Carato
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
 
-export default Footer;
+export default Footer
