0a517f4fde7a95598951c19143a9d0cd46573de7
dpaeza10 <daniela.paez@inchcape.com>
2025-02-12 22:38:20 -0500

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "@/components/Navbar";
+// import { Geist, Geist_Mono } from "next/font/google";
 import  ReactQueryProvider from "@/lib/react-query";
 import "./globals.css";
 
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
