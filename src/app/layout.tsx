c3f1e388218e496634e10ff18154f94460e69f83
dpaeza10 <daniela.paez@inchcape.com>
2025-02-13 15:42:48 -0500

    feat: add Footer component and integrate it into the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 // import { Geist, Geist_Mono } from "next/font/google";
 import  ReactQueryProvider from "@/lib/react-query";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         <ReactQueryProvider>{children}</ReactQueryProvider>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Image from 'next/image';
+import Box from '@mui/material/Box';
+
+const Footer = () => {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <AppBar position="static" sx={{ backgroundColor: 'var(--navbar-bg-color)' }}>
+                <Toolbar>
+                    <Image src="/Logo.svg" alt="Logo" width={70} height={60} />
+                    <Typography variant="body1" color="inherit" sx={{ fontSize: "12px" }}>
+                        © 2025 Carato
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
+
+export default Footer;
